5e03e5ef5f13bc51329ae6480ee73b0bc08da99c
Mahammad Bakr <49376835+mahammadbakr@users.noreply.github.com>
2023-08-27 20:56:12 +0300

    new update...

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,13 +1,70 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  void initState() {
+    print('Init stat is running !');
+    super.initState();
+  }
+
+  @override
+  void didChangeDependencies() {
+    print('didChangeDependencies stat is running !');
+    super.didChangeDependencies();
+  }
+
+  @override
+  void dispose() {
+    print('dispose stat is running !');
+    super.dispose();
+  }
+
+
+  String text = 'init text';
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Home screen'),
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Expanded(
+              child: Text(
+                text,
+                style: TextStyle(fontSize: 20.0),
+              ),
+            ),
+            TextButton(
+              onPressed: () async {
+                final response = await http.get(
+                    Uri.parse('https://jsonplaceholder.typicode.com/users'));
+                print('Response body: ${response.body}');
+
+                setState(() {
+                  text = response.body;
+                });
+              },
+              child: Text(
+                'Fetch Data',
+                style: TextStyle(fontSize: 20.0),
+              ),
+              style: TextButton.styleFrom(
+                primary: Colors.white,
+                backgroundColor: Colors.blue,
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
